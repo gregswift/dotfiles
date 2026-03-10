5088a1823015fb0765141fd95b8b4fbd7c9b0a3b
Greg Swift <gregswift@gmail.com>
2026-03-10 11:01:59 -0500

    feat: Since I prefer podman, set that as the container engine

--- .oh-my-zsh-custom/env-vars.zsh
+++ .oh-my-zsh-custom/env-vars.zsh
@@ -1,1 +1,6 @@
 export EDITOR=nano
+# Disable warnings that podman-compose is running instead of docker-compose
+export PODMAN_COMPOSE_WARNING_LOGS=false
+
+# For my makefiles
+export CONTAINER_ENGINE=podman
