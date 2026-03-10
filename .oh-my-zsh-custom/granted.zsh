22fe066b3e316def835acfa4b67535301d2d2cdb
Greg Swift <gregswift@gmail.com>
2026-03-10 11:05:45 -0500

    feat: make granted setup multi-OS compatible

--- .oh-my-zsh-custom/granted.zsh
+++ .oh-my-zsh-custom/granted.zsh
@@ -1,4 +1,9 @@
-alias assume="source /opt/homebrew/bin/assume"
+# Any OS specific pathing changes
+if [[ $(uname) == "Darwin" ]]; then
+  alias assume="source /opt/homebrew/bin/assume"
+else
+  alias assume="source assume"
+fi
 
 export GRANTED_ENABLE_AUTO_REASSUME=true
 
