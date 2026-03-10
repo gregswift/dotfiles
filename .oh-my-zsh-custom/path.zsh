5cb76c9ce1fa853444a499f0c41d9fd0434658ed
Greg Swift <gregswift@gmail.com>
2026-03-10 11:03:36 -0500

    feat: Set gopath and my custom local bin dir into PATH

--- .oh-my-zsh-custom/path.zsh
+++ .oh-my-zsh-custom/path.zsh
@@ -1,6 +1,7 @@
-# What OS are we running?
-if [[ $(uname) == "Darwin" ]]; then
-  export PATH=${PATH}:${HOME}/.local/bin
-else
+export PATH=${PATH}:${HOME}/.local/bin
+export GOPATH=${HOME}/Development/go
 
-fi
+# Any OS specific pathing changes
+#if [[ $(uname) == "Darwin" ]]; then
+#else
+#fi
