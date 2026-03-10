d7cf596cd3614344fa89c17b7c96e29b8ba29890
Greg Swift <gregswift@gmail.com>
2026-03-10 11:15:39 -0500

    fix: kustomize+helm on private repo requires registry variable to be set otherwise auth fails

--- .oh-my-zsh-custom/env-vars.zsh
+++ .oh-my-zsh-custom/env-vars.zsh
@@ -4,3 +4,13 @@ export PODMAN_COMPOSE_WARNING_LOGS=false
 
 # For my makefiles
 export CONTAINER_ENGINE=podman
+
+
+# Any OS specific pathing changes
+if [[ $(uname) == "Darwin" ]]; then
+  # Configure Helm's registry config file because kustomize overrides most of the
+  # settings, which makes accessing and authenticated helm registry fail
+  export HELM_REGISTRY_CONFIG=~/Library/Preferences/helm/registry/config.json
+else
+  export HELM_REGISTRY_CONFIG=~/.config/helm/registry/config.json
+fi
