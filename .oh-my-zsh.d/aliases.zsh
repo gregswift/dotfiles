6c0f59c60a99f7fe3135ffe66e3647022cd0915c
Greg Swift <gregswift@gmail.com>
2026-03-23 14:02:36 -0500

    feat: adjusted the aliases for slightly better cross platformability/worklife separation

--- .oh-my-zsh.d/aliases.zsh
+++ .oh-my-zsh.d/aliases.zsh
@@ -1,16 +1,25 @@
-alias actionlint="go run github.com/rhysd/actionlint/cmd/actionlint"
-alias cue="go run cuelang.org/go/cmd/cue"
-alias docker="podman"
 alias git-rebase='git-refresh && git rebase -i $(git merge-base $(git branch -r | grep -Po "HEAD -> \K.*$") $(git branch --show-current))'
 alias git-refresh='git fetch --all -p && git rebase $(git branch -r | grep -Po "HEAD -> \K.*$")'
-alias jq="docker run --rm -i apteno/alpine-jq jq"
-alias kube-linter="go run golang.stackrox.io/kube-linter/cmd/kube-linter"
-alias pulumi="docker run -it -e PULUMI_ACCESS_TOKEN -v $(pwd):/app pulumi/pulumi"
-alias sops="docker run --rm -it ghcr.io/getsops/sops:v3.12.1-alpine"
-
 
 git-rebase-branch () {
   git fetch --all -p
   git rebase ${1}
   git-rebase -i $(git merge-base ${1} $(git branch --show-current))
 }
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  # moving these under macos cause i will likely never need most of them on linux this way
+
+  # these are general that i care about
+  alias docker="podman"
+  alias nano="$(brew --prefix nano)/bin/nano"
+
+  # these are typically dayjob oriented
+  alias actionlint="go run github.com/rhysd/actionlint/cmd/actionlint"
+  alias cue="go run cuelang.org/go/cmd/cue"
+  alias kube-linter="go run golang.stackrox.io/kube-linter/cmd/kube-linter"
+  alias pulumi="docker run -it -e PULUMI_ACCESS_TOKEN -v $(pwd):/app pulumi/pulumi"
+  alias sops="docker run --rm -it ghcr.io/getsops/sops:v3.12.1-alpine"
+else
+  alias jq="docker run --rm -i apteno/alpine-jq jq"
+fi
