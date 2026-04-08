e6f1749cf7a282bce178de24cc3309d72a3b3305
Greg Swift <gregswift@gmail.com>
2026-04-08 10:25:04 -0500

    feat: Add support for krew plugins

--- .oh-my-zsh.d/aliases.zsh
+++ .oh-my-zsh.d/aliases.zsh
@@ -8,13 +8,18 @@ git-rebase-branch () {
 }
 
 # Other aliases
-if [[ -f "$(which kubecolor 2> /dev/null)" ]]; then
+KUBECOLOR=$(which kubecolor 2> /dev/null)
+if [[ -f "${KUBECOLOR}" ]]; then
   # If kubecolor is installed, lets use that instead of standard kubectl
-  alias kubectl=kubecolor
+  alias kubectl=${KUBECOLOR}
   compdef kubecolor=kubectl # Make "kubecolor" borrow the same completion logic as "kubectl"
 fi
 alias kc="kubectl"
-
+KREW=$(which kubectl-krew 2> /dev/null)
+if [[ -f "${KREW}" ]]; then
+  alias krew=${KREW}
+  export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+fi
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
   # moving these under macos cause i will likely never need most of them on linux this way
